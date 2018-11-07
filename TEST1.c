03bf420c89ab17ab716237986e06287b426f9b98
mjeong123 <44831769+mjeong123@users.noreply.github.com>
2018-11-07 20:54:02 +0900

    Update TEST1.c

--- TEST1.c
+++ TEST1.c
@@ -1,15 +1,7 @@
 #include "SPI.h"
 #include "WiFi.h"
-//#include <Adafruit_DotStar.h>
 #include <Servo.h> 
 #include <SoftwareSerial.h>
-
-//#define NUMPIXELS 26
-
-//#define DATAPIN    9
-//#define CLOCKPIN   10
-
-//Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
 
 int a = 0;
 int b = 0;
@@ -18,41 +10,39 @@ int b = 0;
   clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
 #endif
 
-char ssid[] = "Bitdol_wifi";       //와이파이 SSID
-char pass[] = "bitdol5124!";   //와이파이 password 
-String location = "Gangnam"; //날씨정보를 보고싶은 지역
-
-//인스턴스 변수 초기화
+char ssid[] = "Bitdol_wifi";   
+char pass[] = "bitdol5124!"; 
+String location = "Gangnam"; 
 
 WiFiServer server(80);
 WiFiClient client;
 
-const unsigned long requestInterval = 60000;  // 요구 시간 딜레이(1 min)
+const unsigned long requestInterval = 60000;
 
 IPAddress hostIp;
 
 uint8_t ret;
-unsigned long lastAttemptTime = 0;            // 마지막으로 서버에서 데이터를 전송받은 시간
-String currentLine = "";          // 서버에서 전송된 데이터 String저장
-String tempString = "";           // 온도 저장 변수
-String humString = "";            // 습도 저장 변수
-String timeString = "";           // 시간 정보 변수
-String pressureString = "";       // 압력 정보 변수
+unsigned long lastAttemptTime = 0;      
+String currentLine = "";       
+String tempString = "";       
+String humString = "";          
+String timeString = "";         
+String pressureString = "";    
 
 String weatherString = "";
 
-boolean readingTemp = false;      //온도 데이터가 있는지 여부 판단
-boolean readingHum = false;       //습도 데이터가 있는지 여부 판단
-boolean readingTime = false;      //시간 데이터가 있는지 여부 판단
-boolean readingPressure = false;  //압력 데이터가 있는지 여부 판단
+boolean readingTemp = false;   
+boolean readingHum = false;     
+boolean readingTime = false;     
+boolean readingPressure = false; 
 
 boolean readingWeather = false;
 
 int temp = 0;
 int weatherInt;
 
-int      head  = 0, tail = -25; // Index of first 'on' and 'off' pixels
-uint32_t color = 0xFF0000;        // 'On' color (starts red)
+int      head  = 0, tail = -25; 
+uint32_t color = 0xFF0000; 
 int i;
 int f = 0;
 Servo servo; 
@@ -60,11 +50,10 @@ int angle = 0;
 
 void setup() {
 
-
-pinMode(9, OUTPUT);
-pinMode(10, OUTPUT);
  
-  //각 변수에 정해진 공간 할당
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+ 
   currentLine.reserve(100);
   tempString.reserve(10);
   humString.reserve(10);
@@ -74,14 +63,14 @@ pinMode(10, OUTPUT);
   
   Serial.begin(115200);    
   delay(10);
-  //WiFi연결 시도
+  
   Serial.println("Connecting to WiFi....");  
-  WiFi.begin(ssid, pass);  //WiFi가 패스워드를 사용한다면 매개변수에 password도 작성
+  WiFi.begin(ssid, pass); 
 
   server.begin();
   Serial.println("Connect success!");
   Serial.println("Waiting for DHCP address");
-  //DHCP주소를 기다린다
+  
   while(WiFi.localIP() == INADDR_NONE) {
     Serial.print(".");
     delay(300);
@@ -90,15 +79,13 @@ pinMode(10, OUTPUT);
   printWifiData();
   connectToServer();
 
-servo.attach(8); 
-
+ servo.attach(8); 
+ 
  pinMode(17, OUTPUT);//A
  pinMode(16, OUTPUT);//B
  pinMode(15, OUTPUT);//C
  pinMode(14, OUTPUT);//D
 
-
-// pinMode(3, OUTPUT);//B
  pinMode(4, OUTPUT);//C
  pinMode(3, OUTPUT);//D
  pinMode(2, OUTPUT);//A
@@ -107,20 +94,16 @@ servo.attach(8);
 void loop()
 {
   if (client.connected()) {
-    while (client.available()) {        //전송된 데이터가 있을 경우 데이터를 읽어들인다.
+    while (client.available()) {   
       
+      char inChar = client.read();    
+      currentLine += inChar; 
       
-      char inChar = client.read();      // 읽어온 데이터를 inChar에 저장한다.
-      
-
-      currentLine += inChar;            //inChar에 저장된 Char변수는 currentLine이라는 String변수에 쌓이게 된다.
-      //라인피드(줄바꿈)문자열이 전송되면 데이터를 보내지 않는다.
       if (inChar == '\n') {
         //Serial.print("clientReadLine = ");
         //Serial.println(currentLine);
         currentLine = "";
       }
-
 
       if(currentLine.endsWith("<weather number=")){
         readingWeather = true;
@@ -130,13 +113,12 @@ void loop()
         if(inChar != 'v'){
           weatherString += inChar;
         }
-
         else{
           readingWeather = false;
           Serial.print("- Weather: ");
           Serial.println(getInt(weatherString));
 
-    if(getInt(weatherString)>200&&getInt(weatherString)<300){ // 번개
+    if(getInt(weatherString)>200&&getInt(weatherString)<300){
    digitalWrite(9, LOW);   
    delay(5);      
    digitalWrite(10, LOW);   
@@ -145,7 +127,7 @@ void loop()
    
    }
 
-  else if(getInt(weatherString)>=300&&getInt(weatherString)<600){ // 비
+  else if(getInt(weatherString)>=300&&getInt(weatherString)<600){
   digitalWrite(9, LOW);   
    delay(5);      
    digitalWrite(10, HIGH);   
@@ -153,7 +135,7 @@ void loop()
   
    }
 
-  else if(getInt(weatherString)>=600&&getInt(weatherString)<782){ // 눈
+  else if(getInt(weatherString)>=600&&getInt(weatherString)<782){ 
  digitalWrite(9,HIGH);   
    delay(5);      
    digitalWrite(10, LOW);   
@@ -161,7 +143,7 @@ void loop()
   
    }
    
-  else if(getInt(weatherString)>783&&getInt(weatherString)<805){ // 맑음
+  else if(getInt(weatherString)>783&&getInt(weatherString)<805){ 
      digitalWrite(9, HIGH);   
    delay(5);      
    digitalWrite(10, HIGH);   
@@ -173,23 +155,21 @@ void loop()
   
       }
       
-      //온도 데이터가 전송되었는지 확인
+      
       if ( currentLine.endsWith("<temperature value=")) {
-        //현재 스트링이 "<temperature value="로 끝났다면 온도데이터를 받을 준비를 한다.
         readingTemp = true; 
         tempString = "";
       }      
-      //<temperature value=뒤에 오는 문자열을 tempString에 저장한다.
       if (readingTemp) {
-        if (inChar != 'm') { //전송될 문자가 'm'이 올때까지 온도값으로 인식
+        if (inChar != 'm') { 
           tempString += inChar;
         } 
-        else { //전송된 문자가 'm'이라면 온도데이터를 그만 저장하고 온도값 출력
+        else { 
           readingTemp = false;
           Serial.print("-  Temperature: ");
           Serial.print(getInt(tempString)-273);
-          Serial.println((char)176);    //degree symbol
-
+          Serial.println((char)176); 
+          
           a = (getInt(tempString)-273)/10;
           b = (getInt(tempString)-273)%10;
           Serial.println(a); 
@@ -197,21 +177,19 @@ void loop()
           Serial.println(b);
         
           PORTC = (int)a;  
-          //PORTD=9;
           PORTD = (int)b;
           delay(10000);
         }
       }
       if ( currentLine.endsWith("<humidity value=")) {
-        //현재 스트링이 "<humidity value ="로 끝났다면 습도 데이터를 받을 준비를 한다.
         readingHum = true; 
         humString = "";
       }
       if (readingHum) { 
-        if (inChar != 'u') {//전송될 문자열이 'u'가 아니라면 계속 습도값을 받게 된다.
+        if (inChar != 'u') {
           humString += inChar;
         } 
-        else { //다음에 전송된 문자열이 'u'라면 습도값을 그만 받고 값을 출력한다.
+        else { 
           readingHum = false;
           Serial.print("-  Humidity: ");
           f = getInt(humString);
@@ -223,7 +201,7 @@ void loop()
 
 
       if (readingTime) {
-        if (inChar != '/') { //다음 전송될 문자가 '/'가 아니라면 계속적으로 시간데이터를 받는다
+        if (inChar != '/') {
           timeString += inChar;
         }
         else {
@@ -234,17 +212,15 @@ void loop()
       }
        
       if ( currentLine.endsWith("<pressure value=")) {
-        // 현재 스트링이 "<pressure value="로 끝났다면 기압 데이터를 받을 준비를 한다.
         readingPressure = true; 
         pressureString = "";
       }
            
       if (readingPressure) {
-        if (inChar != 'u') { //다음 전송될 문자가 'u'가 아니라면 계속 기압데이터를 받는다.
+        if (inChar != 'u') {
           pressureString += inChar;
         } 
-        else { //다음 전송된 문자가 'u'라면 기압데이터를 출력한다.
-
+        else { 
           readingPressure = false;
           Serial.print("-  Pressure: ");
           Serial.print(getInt(pressureString));
@@ -252,23 +228,20 @@ void loop()
 
           currentLine = "";
         }
-      } //pressure 없애면 weather가 안뜸! 이유는 몰게땅ㅇ
-
+      } 
       
-      if ( currentLine.endsWith("</current")) { //현재 스트링이 </current>로 끝났다면 연결을 끊고 다시 서버와 연결을 준비한다.
+      if ( currentLine.endsWith("</current")) {
         Serial.println("Restart");
-        delay(10000); //10초뒤에 서버와 연결을 끊고 재연결을 시도한다.
+        delay(10000); 
         client.stop(); 
         connectToServer();
-        //Serial.println("Disconnected from Server.\n");
-      }
+       }
 
  
     }
   }   
 
   else if (millis() - lastAttemptTime > requestInterval) {
-    //연결을 실패했다면 requestInterval(60초)이후에 다시 연결을 시도한다.
     Serial.println("Fail");
     delay(1000);
     client.stop(); 
@@ -291,24 +264,18 @@ void loop()
   
 }
 
-//서버와 연결
 
 void connectToServer() {
   Serial.println("connecting to server...");
   String content = "";
   if (client.connect(hostIp, 80)) {
     Serial.println("Connected! Making HTTP request to api.openweathermap.org for "+location+"...");
-    //Serial.println("GET /data/2.5/weather?q="+location+"&mode=xml");
    client.println("GET /data/2.5/weather?id=1835329&APPID=d508cb8bf7640cc3aa872218d41760e2&mode=xml");
-   //client.println("GET /data/2.5/weather?q="+location+"&mode=xml");
-    //위에 지정된 주소와 연결한다.
     client.print("HOST: api.openweathermap.org\n");
-    //client.println("User-Agent: launchpad-wifi");
     client.println("Connection: close");
     client.println();
     Serial.println("Weather information for "+location);
    }
-  //마지막으로 연결에 성공한 시간을 기록
   lastAttemptTime = millis();
   Serial.println(lastAttemptTime);
 }
@@ -322,13 +289,11 @@ void printHex(int num, int precision) {
 }
 
 void printWifiData() {
-  // Wifi쉴드의 IP주소를 출력
   Serial.println();
   Serial.println("IP Address Information:");  
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
-  //MAC address출력
   byte mac[6];  
   WiFi.macAddress(mac);
   Serial.print("MAC address: ");
@@ -344,11 +309,9 @@ void printWifiData() {
   Serial.print(":");
   printHex(mac[0], 2);
   Serial.println();
-  //서브넷 마스크 출력
   IPAddress subnet = WiFi.subnetMask();
   Serial.print("NetMask: ");
   Serial.println(subnet);
-  //게이트웨이 주소 출력
   IPAddress gateway = WiFi.gatewayIP();
   Serial.print("Gateway: ");
   Serial.println(gateway);
@@ -362,19 +325,14 @@ void printWifiData() {
   Serial.println("");
 }
 
-
-
-int getInt(String input){  //String데이터를 intger형으로 변환하는 함수
+int getInt(String input){
   int i = 2;
   while(input[i] != '"'){
     i++;
   }
   input = input.substring(2,i);
   char carray[20];
-  //Serial.println(input);
   input.toCharArray(carray, sizeof(carray));
-  //Serial.println(carray);
   temp = atoi(carray);
-  //Serial.println(temp);
   return temp;
 }
